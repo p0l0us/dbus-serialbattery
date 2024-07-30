1508b2ae7b5384703bb21817a2dcd1aea0c3366e
Martin Polehla <p0l0us@users.noreply.github.com>
2024-07-30 05:07:36 +0200

    Update jkbms_can.py

--- etc/dbus-serialbattery/bms/jkbms_can.py
+++ etc/dbus-serialbattery/bms/jkbms_can.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
-from battery import Battery, Cell
+from battery import Battery
 from utils import (
     is_bit_set,
     logger,
